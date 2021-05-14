dc1960113b25e3558a906e0aa290b3e017f91a05
severino de souza souza <severino_souza@outlook.com>
2021-05-14 18:19:56 -0400

    Tratamento de Correções

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,10 +1,9 @@
 class CalcController {
 
-    /**Construtor Display, hora e data (privados).*/
+    /**Construtor*/
     constructor(){
 
         this._audio = new Audio('click.mp3');
-
         this._audioOnOff = false;
 
         this._lastOperator = '';
@@ -12,126 +11,111 @@ class CalcController {
 
         this._operation = [];
         this._locale = 'pt-br';
-        this._displayCalcEl = document.querySelector("#display");//querySelector (da acesso a obj no DOM).
+        this._displayCalcEl = document.querySelector("#display");//querySelector() acesso ao DOM(document).
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
+
         this._currentDate;
+
         this.initialize();
         this.initButtonsEvents();
         this.initKeyboard();
 
     }
+    /**Fim função*/
 
+    /**Função colar N°*/
     pasteFromClipboard(){
 
         document.addEventListener('paste', e => {
-
             let text = e.clipboardData.getData('Text');
-
             this.displayCalc = parseFloat(text);
 
-        })
-
+        });
     }
+    /**Fim função*/
 
+    /**Função copiar N°*/
     copyToClipboard(){
 
         let input = document.createElement('input');
-
         input.value = this.displayCalc;
-
         document.body.appendChild(input);
-
         input.select();
-
         document.execCommand("Copy");
-
         input.remove();
 
     }
+    /**Fim função*/
 
-    /**Inicializa display da culculadora.*/
+    /**Display calculadora*/
     initialize(){
 
-        //Importa função.
         this.setDisplayDateTime();
-
-        //Função intermitente (atualizar a hora a cada segundo em tempo real).
-        setInterval(() => {
-            
+        setInterval(() => {//Função intermitente (atualizar a hora a cada segundo em tempo real).
             this.setDisplayDateTime();
-
         }, 1000);
 
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
         document.querySelectorAll(' .btn-ac').forEach(btn => {
-
             btn.addEventListener('dblclick', e => {
-
                 this.toggleAudio();
-
             });
-
         });
 
     }
-    /**Fim inicializa display.*/
+    /**Fim função*/
 
+    /**Alternancia do audio*/
     toggleAudio(){
 
-        this._audioOnOff = !this._audioOnOff;
+        this._audioOnOff = !this._audioOnOff;//verifica audio (true / false).
 
     }
-    
+    /**Fim alternancia do audio*/
+
+    /**Função toca audio*/
     playAudio(){
 
         if(this._audioOnOff){
-
             this._audio.currentTime = 0;
             this._audio.play();
-
         }
 
     }
-    /**Eventos de teclado.*/
+    /**Fim função*/
+
+    /**Função eventos do teclado*/
     initKeyboard(){
 
         document.addEventListener('keyup', e => {
-
             this.playAudio();
 
             switch (e.key){
-
                 case 'Escape':
                     this.clearAll();
                     break;
-    
+
                 case 'Backspace':
                     this.clearEntry();
                     break;
-    
+
                 case '+':
-    
                 case '-':
-                
                 case '*':
-    
-                case '/':
-                
+                case '/': 
                 case '%':
                     this.addOperation(e.key);
                     break;
 
                 case 'Enter':
-
                 case '=':
                     this.calc();
                     break;
     
                 case '.':
-
                 case ',':
                     this.addDot();
                     break;
@@ -152,64 +136,66 @@ class CalcController {
                 case 'c':
                     if(e.ctrlKey) this.copyToClipboard();
                     break;
-    
             }
-
         });
 
     }
+    /**Fim função*/
 
-    /**Retorna hora e date em tempo real para exibir no display.*/
+    /**Função retorna data e hora*/
     setDisplayDateTime(){
 
         this.displayDate = this.currentDate.toLocaleDateString(this.locale);//toLocaleDateString (Retorna Data)
         this.displayTime = this.currentDate.toLocaleTimeString(this.locale);//toLocaleTimeString (Retorna hora)
         
     }
-    /**Fim retorno hora e data.*/
+    /**Fim função*/
 
-    /**Função click botao (multiplos eventos)*/
+    /**Função evento do click botao (multiplos eventos)*/
     addEventListenerAll(element, events, fn){
 
-        //split() converte strings em array
-        //forEach() recebe o array que contem os eventos e percprre um evento por vez.
-        events.split(' ').forEach(event => {
-
+        events.split(' ').forEach(event => {//split() converte strings em array. forEach() recebe o array que contem os eventos e percprre um evento por vez.
             element.addEventListener(event, fn, false);
-
         });
 
     }
-    /**Fim função click multiplos eventos.*/
+    /**Fim*/
 
     /**Função limpa display.*/
     clearAll(){
 
         this._operation = [];//recebe um array limpo.
-        this._lastNumber = '';
-        this._lastOperator = '';
-
+        this._lastNumber = '';//marca ultimo n° passado.
+        this._lastOperator = '';//marca ultimo operador passado.
         this.setLastNumberToDisplay();
 
     }
-    /**Fim função limpa display.*/
+    /**Fim função*/
 
-    /**Função limpa */
+    /**Função limpa ultimo valor o array*/
     clearEntry(){
 
         this._operation.pop();//pop() (remove valor no fim de array).
-
         this.setLastNumberToDisplay();
 
     }
-    /**Fim função limpa.*/
+    /**Fim função*/
 
-    /** */
+    /**Função p/ pegar ultima possição do array.*/
+    getLastOperation(){
+
+        return this._operation[this._operation.length - 1];
+        
+    }
+    /**Fim função*/
+
+    /**Função defini ultima operação*/
     setLastOperation(value){
 
         this._operation[this._operation.length - 1] = value;
 
     }
+    /**Fim função*/
 
     /**Função troca de operador */
     isOperador(value){
@@ -217,114 +203,78 @@ class CalcController {
         return (['+', '-', '*', '%', '/'].indexOf(value) > -1);//verifica o operador.
 
     }
-    /**Fim função troca de operador.*/
+    /**Fim função*/
 
     /**Função veficica obj no array e calcula se tiver mais que 3 obj.*/
     pushOperation(value){
         
         this._operation.push(value);
-
-        if(this._operation.length > 3){
-            
+        if(this._operation.length > 3){    
             this.calc();
-
         }
 
     }
-    /**Fim função verifica obj.*/
+    /**Fim função*/
 
     getResult(){
 
         try{
-
             return eval(this._operation.join(""));
-
         }catch(e){
-
-            setTimeout(() => {
-
-            }, 1);
-
-            this.setError();
+            setTimeout(() => this.setError(), 1);
 
         }
-
-        
-
     }
 
     /**Função calcula os valores.*/
     calc(){
 
         let last = '';//quarda obj da ultima possição do array.
-
         this._lastOperator = this.getLastItem();
 
         if(this._operation.length < 3 ){
-
             let firstItem = this._operation[0];
-
             this._operation = [firstItem, this._lastOperator, this._lastNumber];
-
         }
 
         if(this._operation.length > 3){
-
             last = this._operation.pop();
-
             this._lastNumber = this.getResult();
-
-        }else if(this._operation.length == 3){
-
+        }else if(this._operation.length === 3){
             this._lastNumber = this.getLastItem(false);  
-
         }
 
-        //join() (converte obj de um array em uma strig).
-        //eval() (função de calcula valores da string).
-        let result = this.getResult();
+        let result = this.getResult();//join() (converte obj de um array em uma strig). eval() (função de calcula valores da string).
 
-        if(last == '%'){//calcula a porcentagem.
-
+        if(last === '%'){//calcula a porcentagem.
             result /= 100;
-
             this._operation = [result];
-
         }else{
-
             this._operation = [result];
 
             if (last) this._operation.push(last);
 
         }
-
         this.setLastNumberToDisplay();
 
     }
-    /**Fim calcula.*/
+    /**Fim função.*/
 
     getLastItem(isOperador = true){
 
         let lastItem;
-
         for(let i = this._operation.length - 1; i >= 0; i--){
 
-            if(this.isOperador(this._operation[i]) == isOperador){
-
+            if(this.isOperador(this._operation[i]) === isOperador){
                 lastItem = this._operation[i];
-
                 break;
-
             }
 
         }
 
         if(!lastItem){
-
             lastItem = (isOperador) ? this._lastOperator : this._lastNumber;
-
         }
-
         return lastItem;
 
     }
@@ -339,7 +289,7 @@ class CalcController {
         this.displayCalc = lastNumber;
 
     }
-    /**Função percorre array.*/
+    /**Fim função*/
 
     /**Função operador.*/
     addOperation(value){
@@ -347,45 +297,26 @@ class CalcController {
         if(isNaN(this.getLastOperation())){//isNaN() (determina se um valor é um número ilegal (não é um número)).
             
             if(this.isOperador(value)){
-
                 this.setLastOperation(value);
-
             }else{
-
                 this.pushOperation(value);
-
                 this.setLastNumberToDisplay();
-
             }
-
 
         }else{
 
             if(this.isOperador(value)){
-
                 this.pushOperation(value);
-
             }else{
-
                 let newValue = this.getLastOperation().toString() + value.toString();//toString() Concatena valores.
-                this._setLastOperation(newValue);//push() (adiciona valor no fim do array).
-
-                //atualiza e exibe numerlo n o display.
-                this.setLastNumberToDisplay();
-
+                this.setLastOperation(newValue);//push() (adiciona valor no fim do array).
+                this.setLastNumberToDisplay();//atualiza e exibe numerlo n o display.
             }
 
         }
         
     }
-    /**Fim operador.*/
-
-    /**Função p/ pegar ultima possição do array.*/
-    getLastOperation(){
-
-        this._operation[this._operation.length-1];
-
-    }
+    /**Fim função*/
 
     /**Função error */
     setError(){
@@ -393,29 +324,23 @@ class CalcController {
         this.displayCalc = "Error";
 
     }
-    /**Fim função error.*/
+    /**Fim*/
 
-    /**Função que add o ponto.*/
+    /**Função que add o ponto*/
     addDot(){
 
         let lastOperation = this.getLastOperation();
 
         if(typeof lastOperation === 'string' && lastOperation.split(' ').indexOf('.') > -1) return;//nao deixa add o ponto a mais.
-
-        if(this.isOperador(lastOperation) || !lastOperation){
-
-            this.pushOperation('0.');
-
+            if(this.isOperador(lastOperation) || !lastOperation){
+                this.setLastOperation('0.');
         }else{
-
             this.setLastOperation(lastOperation.toString() + '.');
-
         }
-
         this.setLastNumberToDisplay();
 
     }
-    /**Fim função ponto.*/
+    /**Fim função*/
 
     /**Ações da calculadora.*/
     execBtn(value){
@@ -423,7 +348,6 @@ class CalcController {
         this.playAudio();
 
         switch (value){
-
             case 'ac':
                 this.clearAll();
                 break;
@@ -475,13 +399,10 @@ class CalcController {
 
             default:
                 this.setError();
-                break;
-
-
         }
 
     }
-    /**Fim ações p/ o botao clicado.*/
+    /**Fim função*/
 
     /**Evento de click dos botoes da calculadora.*/
     initButtonsEvents(){
@@ -491,68 +412,50 @@ class CalcController {
         buttons.forEach((btn, index) => {//forEach (para percorrer cada obj e retornar).
 
             this.addEventListenerAll(btn, "click drag", e => {//addEventListener (Evento de click no botão).
-
                 let textBtn = btn.className.baseVal.replace("btn-","");//Captura  valor do botão.
-
                 this.execBtn(textBtn);
-
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {//Função para mudar de seta para maozinha o cursor do mouse.
-
                 btn.style.cursor = "pointer";
-
             });
 
         })
 
     }
-    /**Fim evento de click.*/
+    /**Fim função*/
 
     /**GET/SET hora exibido no display.*/
     get displayTime(){
-
         return this._timeEl.innerHTML;
-
     }
 
     set displayTime(value){
-
         return this._timeEl.innerHTML = value;
-
     }
     /**Fim GET/SET hora.*/
 
     /**GET/SET data exibido no display.*/
     get displayDate(){
-
         return this._dateEl.innerHTML;
-
     }
 
     set displayDate(value){
-
         return this._dateEl.innerHTML = value;
-
     }
     /**Fim GET/SET data.*/
 
     /**GET/SET valor exibido no display.*/
     get displayCalc(){
-
         return this._displayCalcEl.innerHTML;
-
     }
 
     set displayCalc(value){
 
         if(value.toString().length > 10){
-
             this.setError();
             return false;
-
         }
-        
         this._displayCalcEl.innerHTML = value;
 
     }
@@ -560,15 +463,11 @@ class CalcController {
 
     /**GET/SET data atual exibido em tempo real.*/
     get currentDate(){
-
         return new Date();
-
     }
 
     set CurrentDate(value){
-
         this._currentDate = value;
-
     }
     /**Fim GET/SET data atual.*/
 
